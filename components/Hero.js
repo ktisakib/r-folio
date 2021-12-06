da89667e5d0c88a5ce0ec7317f1bb0fe4c3cd6f5
ktiakib <ktiakib@gmail.com>
2021-12-06 18:49:07 +0600

    hero

---
+++ components/Hero.js
@@ -0,0 +1,10 @@
+const Hero = () => {
+  return (
+    <div className='group bg-hero-pattern  brightness-110 text-white'>
+      
+      <img className='w-full backdrop-filter hover:scale-[1.01]   hover:backdrop-blur-sm transition-all duration-1000  ' src='/img/hero/mask.png'></img>
+    </div>
+  );
+};
+
+export default Hero;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,29 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        "8xl": "90rem",
+      },
+      backgroundImage: {
+        "hero-pattern": "url('/img/hero.png')",
+      },
+      backgroundSize: {
+        auto: "auto",
+        cover: "cover",
+        contain: "contain",
+        "50%": "50%",
+        16: "2rem",
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundSize: ["hover", "focus"],
+      backgroundImage: ["hover", "focus"],
+    },
   },
   plugins: [],
-}
+};
